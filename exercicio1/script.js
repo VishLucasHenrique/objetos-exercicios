e1abe516e0e42900b835d2f1ad7879324902a644
VishLucasHenrique <lucasproenca_11@hotmail.com>
2023-03-23 15:00:11 -0300

    Objetos, Lucas Henrique Proença

---
+++ exercicio1/script.js
@@ -0,0 +1,24 @@
+const sacolao = []
+
+const fruta1 = {
+    nome: 'Banana',
+    preco: 3.59,
+    disponibilidade: true
+}
+
+const fruta2 = {
+    nome: 'Maça',
+    preco: 5.47,
+    disponibilidade: true
+}
+
+const fruta3 = {
+    nome: 'Morango',
+    preco: 7.20,
+    disponibilidade: false
+}
+
+sacolao.push(fruta1, fruta2, fruta3)
+console.log(sacolao)
+
+
